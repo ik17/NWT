1266ced0232a0c0d952d6d24d9541f0734adae39
hannabojadzic <hbojadzic1@etf.unsa.ba>
2019-05-22 14:39:50 +0200

    fixed zuul and async

--- Clanak/src/main/java/com/example/demo/Subscriber.java
+++ Clanak/src/main/java/com/example/demo/Subscriber.java
@@ -1,12 +1,38 @@
 package com.example.demo;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.Entities.Korisnik;
+
+import com.example.demo.Controller.KorisnikController;
+import com.example.demo.Entities.Korisnik;
 
 @Component
 public class Subscriber {
-	@RabbitListener(queues="spring-boot-3")
-	public void receivedMessage(String msg) {
-		System.out.println("Received message " + msg);
+	@Autowired
+	KorisnikController korisnikController;
+	
+	@RabbitListener(queues="spring-boot-5")
+	public void receivedMessage(String message) throws Exception {
+		System.out.println("Received message " + message);
+		 String action = message.substring(0,1);
+	        String data = message.substring(1);
+	        System.out.println(action);
+	        if(action.equals("1")) {
+	        	//System.out.println(action);
+	        	korisnikController.createUser(new Korisnik(data), null);
+	        }
+	        else if( action.equals( "2")) {
+	        	korisnikController.updateKorisnik(Long.parseLong(data.substring(0,1),10), new Korisnik(data.substring(1)), null);
+	        }
+	        else  {
+	        	//kC.deleteCategory(Long.parseLong(data,10));
+	        	System.out.println(Long.parseLong(data,10));
+	        	korisnikController.deleteUser(Long.parseLong(data,10));
+	        	
+	        }
 	}
 }
+
--- HistorijaClanak/src/main/java/com/example/demo/Subscriber.java
+++ HistorijaClanak/src/main/java/com/example/demo/Subscriber.java
@@ -1,12 +1,35 @@
 package com.example.demo;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.controller.KorisnikController;
+import com.example.demo.entity.Korisnik;
 
 @Component
 public class Subscriber {
-	@RabbitListener(queues="spring-boot-3")
-	public void receivedMessage(String msg) {
-		System.out.println("Received message " + msg);
+	 @Autowired
+	    KorisnikController kC;
+	
+	@RabbitListener(queues="spring-boot-6")
+	public void receivedMessage(String message) throws Exception {
+		System.out.println("Received message " + message);
+		String action = message.substring(0,1);
+        String data = message.substring(1);
+        System.out.println(action);
+        if(action.equals("1")) {
+        	//System.out.println(action);
+        kC.createUder(new Korisnik(data), null);
+        }
+        else if( action.equals( "2")) {
+        	kC.updateUser(Long.parseLong(data.substring(0,1),10), new Korisnik(data.substring(1)), null);
+        }
+        else  {
+        	//kC.deleteCategory(Long.parseLong(data,10));
+        	System.out.println(Long.parseLong(data,10));
+        	kC.deleteCategory(Long.parseLong(data,10));
+        	
+        }
 	}
 }
