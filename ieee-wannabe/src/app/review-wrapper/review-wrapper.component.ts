ac77f9256351d0854496f7e5e2d387588822d32a
Armin <asarak1@etf.unsa.ba>
2019-05-29 06:06:52 +0200

    review komponenta

--- ieee-wannabe/src/app/review-wrapper/review-wrapper.component.ts
+++ ieee-wannabe/src/app/review-wrapper/review-wrapper.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewWrapperComponent implements OnInit {
 
+  nazivClanka: string = "Članak 1";
+  autori: string[] = [ "Autor 1", "Autor 2", "Autor 3" ];
+  kategorija: string = "Kategorija 1";
+  urlZaPreuzimanje: string = "https://www.google.com/";
+  komentar: string = "";
+  komentari: any[] = [ { "korisnik":"user1", tekst:"ovo je moj komentar na ovo"},
+                       { "korisnik":"user2", tekst:"a ovo je moj komentar na ovo"},            
+                       { "korisnik":"user123", tekst:"slazem se sa user1"} ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showAuthors(): string {
+    return this.autori.join(", ");
   }
 
 }
